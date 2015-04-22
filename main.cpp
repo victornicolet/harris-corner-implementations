5bfd3285d305a85581b55a9a9c15ebfea8034072
victornicolet <victor.nicolet.0@gmail.com>
2015-04-22 13:27:41 +0200

    Micro add

--- main.cpp
+++ main.cpp
@@ -32,7 +32,7 @@ int main(int argc, char ** argv)
 
   if ( !image.data )
   {
-      printf("No image data \n");
+      printf("No image data ! Are you sure %s is an image ?\n", argv[1]);
       return -1;
   }
 
